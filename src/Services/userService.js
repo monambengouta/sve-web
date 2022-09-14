37d182357a0093e689c48c93f1daaf145ea4c479
monaambengouta <monaam.bengouta@groupado.com>
2022-09-14 15:10:22 +0100

    change syntax to ECS61.0.0

--- src/Services/userService.js
+++ src/Services/userService.js
@@ -1,18 +1,16 @@
-const  axios = require("axios");
+import axios  from "axios";
 
- const dologin =  async (credential)=>{
-    const baseURL = "http://localhost:3000";
-    let data = {
-        email: credential.email,
-        password: credential.password,
-      };
-      // eslint-disable-next-line no-undef
-    return await axios.post(baseURL + "/loginClient", data,{
-        headers: {
-          "Content-Type": "application/json",
-        }
-    });
-}
-module.exports = {
-    dologin
-}
+const dologin = async (credential) => {
+  const baseURL = "http://localhost:3000";
+  let data = {
+    email: credential.email,
+    password: credential.password,
+  };
+  // eslint-disable-next-line no-undef
+  return await axios.post(baseURL + "/loginClient", data, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
+export { dologin };
